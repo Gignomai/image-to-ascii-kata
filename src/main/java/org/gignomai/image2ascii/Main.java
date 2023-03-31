5689aa82175cddb2bfe3ed9232e53748362f7439
Manel Márquez <gignomaicsl@google.com>
2023-03-31 23:04:40 +0200

    First commit. Convert an inmage to ascii art

---
+++ src/main/java/org/gignomai/image2ascii/Main.java
@@ -0,0 +1,46 @@
+package org.gignomai.image2ascii;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+public class Main {
+
+    public static final String IMAGE_FILE_NAME = "1854_40px.jpg";
+
+    public static void main(String[] args) {
+
+        try {
+            Optional<InputStream> resourceAsStream = Optional.ofNullable(Main.class.getClassLoader().getResourceAsStream(IMAGE_FILE_NAME));
+            BufferedImage image = ImageIO.read(resourceAsStream.orElseThrow(IOException::new));
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < image.getHeight(); i++) {
+                for (int j = 0; j < image.getWidth(); j++) {
+                    result.append(getPixelDarknessAsAsciiChar(image.getRGB(j, i))).append(" ");
+                }
+                result.append("\n");
+            }
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static char getPixelDarknessAsAsciiChar(int pixel) {
+        char[] map = " .,:;ox%#@".toCharArray();
+
+        int red = (pixel >> 16) & 0xff;
+        int green = (pixel >> 8) & 0xff;
+        int blue = (pixel) & 0xff;
+
+        int index = ((red + green + blue) / 3) / 25;
+
+        if (index < 0) index = 0;
+        if (index > 9) index = 9;
+
+        return map[index];
+    }
+
+}
